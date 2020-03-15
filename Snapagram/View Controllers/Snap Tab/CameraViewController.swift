ce4257b6b1af3c07813f4efacc4c12168c6a3500
Yinan Chen <yinan_chen@berkeley.edu>
2020-03-15 15:20:22 -0700

    add controller

--- Snapagram/View Controllers/Snap Tab/CameraViewController.swift
+++ Snapagram/View Controllers/Snap Tab/CameraViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class CameraViewController: UIViewController {
 
+    @IBOutlet weak var selectedImage: UIImageView!
+    var imagePickerController: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        self.cameraImagePicker = UIImagePickerController()
+//        self.cameraImagePicker.delegate = self
+//        self.cameraImagePicker.sourceType = .camera
+        
+        self.imagePickerController = UIImagePickerController()
+        self.imagePickerController.delegate = self
+        self.imagePickerController.sourceType = .photoLibrary
 
         // Do any additional setup after loading the view.
     }
     
     
+    @IBAction func albumBtnPressed(_ sender: Any) {
+        present(imagePickerController,animated: true){
+            print("Album Image Picker Presented")
+        }
+    }
+}
 
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let img = info[.originalImage] as? UIImage{
+            selectedImage.image = img
+        }
+        
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
 }
