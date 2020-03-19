2816bae839ea03b58a69be172369f33c15429ae4
Yinan Chen <Sherry@Yinans-MacBook-Pro.local>
2020-03-19 16:41:25 -0700

    emoji table view works

--- Snapagram/View Controllers/Snap Tab/PostViewController.swift
+++ Snapagram/View Controllers/Snap Tab/PostViewController.swift
@@ -8,27 +8,48 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UICollectionViewDataSource {
 
     @IBOutlet weak var postImgView: UIImageView!
     var postImg: UIImage!
+    var feedData : FeedData!
+    
+    //reference to whole collection view
+    @IBOutlet weak var threadCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //load img transferred from segue
         postImgView.image = postImg
+        
+        //require delegates to connect between view and controller
+        threadCollectionView.dataSource = self
+        self.feedData = FeedData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // obey the set protocols
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return number of cell
+        return feedData.threads.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let index = indexPath.item
+        //get the specific thread that are working on
+        let thread = feedData.threads[index]
+        
+        //get reference to the cell at the above index
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postViewThreadCell", for: indexPath) as? PostViewThreadCollectionViewCell{
+            cell.emojiLabel.text = thread.emoji
+            cell.nameLabel.text = thread.name
+            
+            return cell
+        }else{
+            //return empty UICollectionViewCell
+            return UICollectionViewCell()
+        }
+        
+    }
 }
---
+++ Snapagram/View/PostViewThreadCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostViewThreadCollectionViewCell.swift
+//  Snapagram
+//
+//  Created by Yinan Chen on 3/19/20.
+//  Copyright © 2020 iOSDeCal. All rights reserved.
+//
+
+import UIKit
+
+// connect cells to code 
+class PostViewThreadCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+}
