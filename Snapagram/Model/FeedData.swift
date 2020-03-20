3e541055217f71e08c74f182ec9f99349bc32715
Yutong Zhou <yz327@cornell.edu>
2020-03-19 22:47:29 -0700

    part 2 done

--- Snapagram/Model/FeedData.swift
+++ Snapagram/Model/FeedData.swift
@@ -8,9 +8,12 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 // Create global instance of the feed
 var feed = FeedData()
+let db = Firestore.firestore()
+let storage = Storage.storage()
 
 class Thread {
     var name: String
@@ -25,6 +28,55 @@ class Thread {
     
     func addEntry(threadEntry: ThreadEntry) {
         entries.append(threadEntry)
+        let imageID = UUID.init().uuidString
+        let user = threadEntry.username
+        
+        let storageRef = storage.reference(withPath: "threadEntry/\(imageID).jpg")
+        guard let imageData = threadEntry.image.jpegData(compressionQuality: 0.75) else { return }
+        let uploadMetadata = StorageMetadata.init()
+        uploadMetadata.contentType = "image/jpeg"
+        storageRef.putData(imageData)
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("threadEntry").addDocument(data:[
+            "imageID": imageID,
+            "user": user]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+    }
+    
+    func fetch() {
+        db.collection("threadEntry").getDocuments(){ (threadEntries, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                
+                var username: String
+                var imageID: String
+                
+                for document in threadEntries!.documents {
+                    imageID = document.data()["imageID"] as! String
+                    username = document.data()["user"] as! String
+                    
+                    let storageRef = storage.reference(withPath: "threadEntry/\(imageID).jpg")
+                    
+                    storageRef.getData(maxSize: 4 * 1024 * 1024) { (data, error) in
+                        if error != nil {
+                            print("error")
+                        }
+                        if let data = data {
+                            let image = UIImage(data: data)
+                            self.entries.append(ThreadEntry(username: username, image: image!))
+                        }
+                    }
+                }
+            }
+        }
     }
     
     func removeFirstEntry() -> ThreadEntry? {
@@ -53,7 +105,7 @@ struct Post {
 }
 
 class FeedData {
-    var username = "YOUR USERNAME"
+    var username = "Yinan&Yutong"
     
     var threads: [Thread] = [
         Thread(name: "memes", emoji: "😂"),
@@ -75,12 +127,37 @@ class FeedData {
     init() {
         for thread in threads {
             let entry = ThreadEntry(username: self.username, image: UIImage(named: "garbers")!)
-            thread.addEntry(threadEntry: entry)
+            thread.entries.append(entry)
         }
     }
     
     func addPost(post: Post) {
         posts.append(post)
+        let location = post.location
+        let imageID = UUID.init().uuidString
+        let user = post.user
+        let caption = post.caption
+        let date = Timestamp(date: post.date)
+        
+        let storageRef = storage.reference(withPath: "post/\(imageID).jpg")
+        guard let imageData = post.image!.jpegData(compressionQuality: 0.75) else { return }
+        let uploadMetadata = StorageMetadata.init()
+        uploadMetadata.contentType = "image/jpeg"
+        storageRef.putData(imageData)
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("post").addDocument(data:[
+            "location": location,
+            "imageID": imageID,
+            "user": user,
+            "caption": caption,
+            "date": date]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
     }
     
     // Optional: Implement adding new threads!
