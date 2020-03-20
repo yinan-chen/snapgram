207ba18fe1380928599003af626c0ff612b37e8a
Yinan Chen <Sherry@Yinans-MacBook-Pro.local>
2020-03-19 17:17:43 -0700

    add thread post function

--- Snapagram/View Controllers/Snap Tab/PostViewController.swift
+++ Snapagram/View Controllers/Snap Tab/PostViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class PostViewController: UIViewController, UICollectionViewDataSource {
+class PostViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
 
     @IBOutlet weak var postImgView: UIImageView!
     var postImg: UIImage!
-    var feedData : FeedData!
+    var selectedThreadIndex: Int!
     
     //reference to whole collection view
     @IBOutlet weak var threadCollectionView: UICollectionView!
@@ -24,21 +24,20 @@ class PostViewController: UIViewController, UICollectionViewDataSource {
         postImgView.image = postImg
         
         //require delegates to connect between view and controller
+        threadCollectionView.delegate = self
         threadCollectionView.dataSource = self
-        self.feedData = FeedData()
     }
-    
     
     // obey the set protocols
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //return number of cell
-        return feedData.threads.count
+        return feed.threads.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let index = indexPath.item
         //get the specific thread that are working on
-        let thread = feedData.threads[index]
+        let thread = feed.threads[index]
         
         //get reference to the cell at the above index
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postViewThreadCell", for: indexPath) as? PostViewThreadCollectionViewCell{
@@ -51,5 +50,19 @@ class PostViewController: UIViewController, UICollectionViewDataSource {
             return UICollectionViewCell()
         }
         
+    }
+    
+    // function for selected thread view
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedThreadIndex = indexPath.item
+    }
+    
+    @IBAction func createThreadBtnPressed(_ sender: Any) {
+        let newThreadEntry = ThreadEntry(username: feed.threads[selectedThreadIndex].name, image: postImg)
+    feed.threads[selectedThreadIndex].addEntry(threadEntry: newThreadEntry)
+        print("add new thread entry to \(newThreadEntry.username)")
+    }
+    
+    @IBAction func createPostBtnPressed(_ sender: Any) {
     }
 }
